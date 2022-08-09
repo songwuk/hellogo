9164cb5b18d92772ae07ebef21045b314affeb8e
songwuk <1017301325@qq.com>
2022-08-09 15:27:06 +0800

    fix: update

--- two/two.go
+++ two/two.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 
 	"example.com/one"
+
+	"log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := one.Hello("Gladys")
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// // Get a greeting message and print it.
+	// message := one.Hello("Gladys")
+	// fmt.Println(message)
+
+	// Request a greeting message.
+	message, err := one.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
